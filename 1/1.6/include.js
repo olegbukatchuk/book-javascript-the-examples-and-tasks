22d253251cbaedb43e53e1151858fc23285519f5
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-25 21:51:33 +0300

    ok

---
+++ 1/1.6/include.js
@@ -0,0 +1,3 @@
+var x="3+(5*2+6)/4"
+document.write(x+" = ")
+document.write(eval(x))
---
+++ 2/2.1/include.js
@@ -0,0 +1,9 @@
+var txt,name
+txt="Давайте познакомимся! Как вас зовут?"
+name=prompt(txt)
+
+if(name=="") {
+    document.write("Жаль, но вы так и не представились")
+} else {
+    document.write("Приятно познакомится "+name+"!")
+}
