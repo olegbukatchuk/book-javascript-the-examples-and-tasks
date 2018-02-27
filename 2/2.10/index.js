5449c91398a3fe8f66072cfe19012becf6b8ff32
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-02-27 22:55:42 +0300

    ok

---
+++ 2/2.10/index.js
@@ -0,0 +1,12 @@
+var n=100,s=0,k=1
+var txt="1<sup>2</sup> + 2<sup>2</sup> + ... + "
+txt+=n+"<sup>2</sup> = "
+
+for(;;) {
+    s+=k*k
+    k++
+    if(k>n) {
+        break
+    }
+}
+document.write(txt+s)
