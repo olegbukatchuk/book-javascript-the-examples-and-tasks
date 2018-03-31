2de9a7d3fcc5014b29bd62a48d3ad0f365f34b6d
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-03-31 23:46:13 +0300

    ok

---
+++ 3/3.2/index.js
@@ -0,0 +1,14 @@
+var myText="Глвальная переменная"
+
+document.write(myText+"<br>")
+
+function show() {
+    var myText
+    myText="Локальная переменная"
+
+    document.write(myText+"<br>")
+}
+
+show()
+
+document.write(myText+"<br>")
