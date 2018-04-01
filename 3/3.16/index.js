d7d51bee0fd7f4846f0bd05112343100223592df
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-01 22:56:57 +0300

    ok

---
+++ 3/3.16/index.js
@@ -0,0 +1,19 @@
+function first() {
+    document.write("Первая функция<br>")
+}
+
+function second() {
+    document.write("Вторая функция<br>")
+}
+
+first()
+second()
+
+var tmp
+
+tmp = first
+first = second
+second = tmp
+
+first()
+second()
