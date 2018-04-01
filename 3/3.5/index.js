5a79c6806fe26fe5b8036a0060211fb19cd84546
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-01 22:15:09 +0300

    ok

---
+++ 3/3.5/index.js
@@ -0,0 +1,14 @@
+var myText = "Глобальная переменная"
+
+document.write(myText + "<br>")
+
+function show() {
+    myText="Переменная с намёком на глобальность"
+    document.write(myText+"<br>")
+    var myText="Локальная переменная"
+    document.write(myText + "<br>")
+}
+
+show()
+
+document.write(myText + "<br>")
