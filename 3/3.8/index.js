74a8a0edf6c4bdea44cca2c6f8cd0dca60a52700
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-01 22:24:13 +0300

    ok

---
+++ 3/3.8/index.js
@@ -0,0 +1,13 @@
+function f(x) {
+    document.write("Аргумент: " + x + "<br>")
+    x++
+    document.write("Аргумент: " + x + "<br>")
+}
+
+var num = 100
+
+document.write("Переменная: " + num + "<br>")
+
+f(num)
+
+document.write("Переменная: " + num + "<br>")
