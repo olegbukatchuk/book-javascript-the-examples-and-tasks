677946a9f8f90afb785d1a3cc58f64e677fcbae0
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 23:41:00 +0300

    ok

--- 3/3.17/index.js
+++ 3/3.17/index.js
@@ -1,9 +1,9 @@
 var f = function(msg) {
-    document.write(msg + "<br>")
-}
+    document.write(msg + "<br>");
+};
 
-f("Анонимная функция")
+f("Анонимная функция");
 
 (function(msg) {
-    document.write("<b>" + msg + "</b><br>")
-} ("Ещё одна функция"))
+    document.write("<b>" + msg + "</b><br>");
+} ("Ещё одна функция"));
--- 3/3.19/index.js
+++ 3/3.19/index.js
@@ -1,15 +1,15 @@
 function makePolynom(a, b, c) {
     return function(x) {
-        return a + b * x + c * x * x
+        return a + b * x + c * x * x;
     }
 }
 
-var P,Q
+var P,Q;
 
-P = makePolynom(1, 2, 1)
-Q = makePolynom(2, -1, 1)
+P = makePolynom(1, 2, 1);
+Q = makePolynom(2, -1, 1);
 
-var z=2
+var z=2;
 
-document.write("P(" + z + ") = " + P(z) + "<br>")
-document.write("Q(" + z + ") = " + Q(z) + "<br>")
+document.write("P(" + z + ") = " + P(z) + "<br>");
+document.write("Q(" + z + ") = " + Q(z) + "<br>");
