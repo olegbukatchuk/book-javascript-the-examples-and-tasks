d9011e1d972eac2b0cdcf6d8ee17183a5593d7cf
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 23:41:25 +0300

    ok

--- 3/3.20/index.js
+++ 3/3.20/index.js
@@ -1,17 +1,17 @@
 function makeFibs() {
-    var a = 0, b = 1
-    return next
+    var a = 0, b = 1;
+    return next;
 
     function next() {
-        b = a + b
-        a = b - a
+        b = a + b;
+        a = b - a;
 
-        return a
+        return a;
     }
 }
 
-var nextFib = makeFibs()
+var nextFib = makeFibs();
 
 for (var k = 1; k <= 15; k++) {
-    document.write(nextFib() + " | ")
+    document.write(nextFib() + " | ");
 }
