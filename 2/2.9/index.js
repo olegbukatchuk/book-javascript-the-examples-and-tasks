da70f51912cc4d9964d8db361c1f06d5a0f1fc1a
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 22:39:56 +0300

    ok

--- 2/2.9/index.js
+++ 2/2.9/index.js
@@ -1,6 +1,8 @@
 var n = 100, s = 0, k = 1;
-var txt="1<sup>2</sup> + 2<sup>2</sup> + ... + "
-txt+=n+"<sup>2</sup> = "
+var txt = "1<sup>2</sup> + 2<sup>2</sup> + ... + ";
 
-for(;k<=n;s+=k*k,k++);
-document.write(txt+s)
+txt += n + "<sup>2</sup> = ";
+
+for (; k <= n; s += k * k, k++) {}
+
+document.write(txt + s);
