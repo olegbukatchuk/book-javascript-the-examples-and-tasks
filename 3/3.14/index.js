8876a669cae7afc24cd8fbea6fa6da73b79fac99
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 23:25:04 +0300

    ok

--- 3/3.14/index.js
+++ 3/3.14/index.js
@@ -1,23 +1,23 @@
 function mlog(x) {
-    var s = 0, k = 1, q = x
-    var n = 100
+    var s = 0, k = 1, q = x;
+    var n = 100;
 
     while(k <= n) {
-        doNext()
+        doNext();
     }
 
-    return s
+    return s;
 
     function doNext() {
-        s += q / k
-        q *= (-1) * x
-        k++
+        s += q / k;
+        q *= (-1) * x;
+        k++;
     }
 }
 
-document.write("<h4>Использование внутренней функции</h4>")
+document.write("<h4>Использование внутренней функции</h4>");
 
-var x = 0.5
+var x = 0.5;
 
-document.write("ln(1 + " + x + ") = " + mlog(x) + "<br>")
-document.write("Проверка: " + Math.log(1 + x) + "<br>")
+document.write("ln(1 + " + x + ") = " + mlog(x) + "<br>");
+document.write("Проверка: " + Math.log(1 + x) + "<br>");
