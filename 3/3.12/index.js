e64791cb0af118354375727f53c5849de9508f09
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 23:13:06 +0300

    ok

--- 3/3.12/index.js
+++ 3/3.12/index.js
@@ -1,13 +1,13 @@
 function fibs(n) {
     if(n == 1 || n == 2) {
-        return 1
+        return 1;
     } else {
-        return fibs(n - 1) + fibs(n - 2)
+        return fibs(n - 1) + fibs(n - 2);
     }
 }
 
-document.write("<h4>Числа Фибоначчи</h4>")
+document.write("<h4>Числа Фибоначчи</h4>");
 
 for (var k = 1; k <= 10; k++) {
-    document.write(fibs(k) + " ")
+    document.write(fibs(k) + " ");
 }
--- 3/3.13/index.js
+++ 3/3.13/index.js
@@ -1,27 +1,27 @@
 function mlog(x) {
-    var s=0, k
-    var n = 100
+    var s=0, k;
+    var n = 100;
 
     for (k = 1; k <= n; k++) {
-        s -= power(-x, k) / k
+        s -= power(-x, k) / k;
     }
 
-    return s
+    return s;
 
     function power(z, m) {
-        var p=1, i
+        var p=1, i;
 
         for(i = 1; i <= m; i++) {
-            p *= z
+            p *= z;
         }
 
-        return p
+        return p;
     }
 }
 
-document.write("<h4>Использование ынутренней функции</h4>")
+document.write("<h4>Использование ынутренней функции</h4>");
 
-var x = 0.5
+var x = 0.5;
 
-document.write("ln(1 + "+ x +") = " + mlog(x) + "<br>")
-document.write("Проверка: "+ Math.log(1 + x) + "<br>")
+document.write("ln(1 + "+ x +") = " + mlog(x) + "<br>");
+document.write("Проверка: "+ Math.log(1 + x) + "<br>");
