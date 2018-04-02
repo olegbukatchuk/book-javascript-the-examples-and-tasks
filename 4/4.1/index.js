2ea1a8d4f5639b6673b974b8448a4d51e504dff7
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 22:07:27 +0300

    ok

---
+++ 4/4.1/index.js
@@ -0,0 +1,10 @@
+var obj = {name: "Иван Петров", age: 38}
+
+document.write("<b>Имя</b>: " + obj.name + "<br>")
+document.write("<b>Возраст</b>: " + obj.age + "<br>")
+
+obj.name = "Петр Иванов"
+obj.age++
+
+document.write("<b>Имя</b>: " + obj.name + "<br>")
+document.write("<b>Возраст</b>: " + obj.age + "<br>")
