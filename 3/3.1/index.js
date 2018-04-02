bff6e5d80b4a9150604d7c9e79c67c4db1fe9275
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-02 23:03:02 +0300

    ok

--- 3/3.1/index.js
+++ 3/3.1/index.js
@@ -1,20 +1,22 @@
-makeHeader("Знакомимся с функциями")
+makeHeader("Знакомимся с функциями");
 
 function getRandText(n) {
-    var txt="Случайные целые числа (от 1 до 10):<br>*"
-    for(var k=1;k<=n;k++) {
-        txt+=myRand()+" * "
+    var txt = "Случайные целые числа (от 1 до 10):<br>*";
+
+    for (var k = 1; k <= n; k++) {
+        txt += myRand() + " * ";
     }
-    txt+="<br>"
-    return txt
+
+    txt += "<br>";
+    return txt;
 }
 
-document.write(getRandText(20))
+document.write(getRandText(20));
 
 function myRand() {
-    return 1+Math.floor(10*Math.random())
+    return 1 + Math.floor(10 * Math.random());
 }
 
 function makeHeader(t) {
-    document.write("<h4>"+t+"</h4>")
+    document.write("<h4>" + t + "</h4>");
 }
