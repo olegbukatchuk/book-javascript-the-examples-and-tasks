4f2167e8938aaff48cd535cd292f84ec67fe8103
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-07 23:35:40 +0300

    ok

---
+++ 4/4.8/index.js
@@ -0,0 +1,11 @@
+document.write("<h4>Цвета</h4>")
+
+var colors = {
+    red: "красный",
+    yellow: "жёлтый",
+    green: "зелёный"
+}
+
+for (var s in colors) {
+    document.write(s + " - " + colors[s] + "<br>")
+}
