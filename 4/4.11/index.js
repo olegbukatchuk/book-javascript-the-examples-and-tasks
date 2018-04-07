ef52fa956ddee55a4a2ff0f89bc126323f3129f0
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-07 23:51:16 +0300

    ok

---
+++ 4/4.11/index.js
@@ -0,0 +1,41 @@
+var X = {
+    color: "желтый",
+    number: 123,
+    show: function(arg) {
+        document.write("<b>" + arg + "</b>: ")
+        for (var s in this) {
+            if (s != "show") {
+                document.write(s + " -> " + this[s] + " | ")
+            }
+        }
+        document.write("<br>")
+    }
+};
+
+var A = Object.create(X);
+var B = Object.create(X);
+
+showAll();
+
+A.color = "желтый";
+A.number = 321;
+B.color = "зелёный";
+
+showAll();
+
+X.name = "прототип";
+A.state = true;
+
+showAll();
+
+delete X.number;
+delete A.color;
+
+showAll();
+
+function showAll() {
+    X.show("X");
+    A.show("A");
+    B.show("B");
+    document.write("<hr>")
+}
