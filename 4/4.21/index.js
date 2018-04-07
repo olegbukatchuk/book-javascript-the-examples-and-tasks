fa6f478fa8466813f0aead23a4c64651fef864f4
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-08 00:43:35 +0300

    ok

---
+++ 4/4.21/index.js
@@ -0,0 +1,25 @@
+var A = {
+    name:"объект A",
+    n: 0
+};
+
+Object.defineProperty(
+    A,
+    "number",
+    {
+        get: function() {
+            return this.n % 10;
+            },
+        set: function(x) {
+            this.n = (x % 10)
+        }
+    });
+
+A.number = 123;
+document.write(A.name + "<br>" + A.number + "<hr>");
+
+A.number = 5;
+document.write(A.name + "<br>" + A.number + "<hr>");
+
+A.n = 12;
+document.write(A.name + "<br>" + A.number + "<hr>");
