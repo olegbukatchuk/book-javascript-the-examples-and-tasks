1cf8a501c9a8d5acebce9d4a711fb6b9b28fd5bb
Oleg Bukatchuk <oleg@bukatchuk.com>
2018-04-10 00:45:40 +0300

    ok

---
+++ 6/6.15/index.js
@@ -0,0 +1,18 @@
+var myTrue = new Boolean(true);
+var myFalse = new Boolean(false);
+
+if (myTrue) {
+    document.write("Объект myTrue<br>");
+}
+
+if (myTrue == true) {
+    document.write("Снова объект myTrue<br>");
+}
+
+if (myFalse) {
+    document.write("Объект myFalse<br>");
+}
+
+if (myFalse == false) {
+    document.write("Снова объект myFalse");
+}
